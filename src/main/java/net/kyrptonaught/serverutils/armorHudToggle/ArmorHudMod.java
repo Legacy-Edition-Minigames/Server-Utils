59e866c5944eb938c9a4a927014cdee2f4d92db9
Virusnest <kai.maloney.au@gmail.com>
2023-06-07 12:36:31 +1000

    ArmorHud State Command

--- src/main/java/net/kyrptonaught/serverutils/armorHudToggle/ArmorHudMod.java
+++ src/main/java/net/kyrptonaught/serverutils/armorHudToggle/ArmorHudMod.java
@@ -12,20 +12,24 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 
+import java.util.Collection;
+
 public class ArmorHudMod extends Module{
     public static final Identifier ARMOR_HUD_STATE = new Identifier("lem", "armor_hud_state_packet");
 
     public void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("armorHud")
                 .requires((source) -> source.hasPermissionLevel(2))
-                .then(CommandManager.argument("entity", EntityArgumentType.player()).then(CommandManager.argument("state", BoolArgumentType.bool())
+                .then(CommandManager.argument("entity", EntityArgumentType.players()).then(CommandManager.argument("state", BoolArgumentType.bool())
                         .executes(context -> {
-                            ServerPlayerEntity entity = EntityArgumentType.getPlayer(context, "entity");
+                            Collection<ServerPlayerEntity> entity = EntityArgumentType.getPlayers(context, "entity");
                             boolean state = BoolArgumentType.getBool(context, "state");
                             PacketByteBuf buf = PacketByteBufs.create();
                             buf.writeBoolean(state);
                             if (entity!=null)
-                                ServerPlayNetworking.send(entity, ARMOR_HUD_STATE,buf);
+                                for (ServerPlayerEntity player : entity) {
+                                    ServerPlayNetworking.send(player, ARMOR_HUD_STATE, buf);
+                                }
                             return 1;
                         }))));
     }
