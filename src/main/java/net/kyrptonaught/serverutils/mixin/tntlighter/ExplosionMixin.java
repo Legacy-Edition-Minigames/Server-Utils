3d9a131fc72958437f21126f8ac6cb906181a349
kyrptonaught <anthonybuttillo10@gmail.com>
2023-07-18 17:42:21 -0400

    Fix tntlighter

--- src/main/java/net/kyrptonaught/serverutils/mixin/tntlighter/ExplosionMixin.java
+++ src/main/java/net/kyrptonaught/serverutils/mixin/tntlighter/ExplosionMixin.java
@@ -3,6 +3,8 @@ package net.kyrptonaught.serverutils.mixin.tntlighter;
 import net.kyrptonaught.serverutils.tntlighter.TNTLighter;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.explosion.Explosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,8 +18,8 @@ import java.util.Set;
 @Mixin(Explosion.class)
 public class ExplosionMixin {
 
-    @Inject(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/World;getOtherEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Box;)Ljava/util/List;"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void dontEffectItemEntities(CallbackInfo ci, Set set, float q, int k, int l, int r, int s, int t, int u, List<Entity> list) {
+    @Inject(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I"), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void dontEffectItemEntities(CallbackInfo ci, Set<BlockPos> set, int i, float q, int k, int l, int r, int s, int t, int u, List<Entity> list) {
         if (TNTLighter.ENABLED)
             list.removeIf(entity -> entity instanceof ItemEntity);
     }
