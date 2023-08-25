e45b75b2b2a188c74f4e80967bbf92ca3a175d70
kyrptonaught <anthonybuttillo10@gmail.com>
2023-08-24 21:07:03 -0400

    Actually remove hard dependency on Fabric Proxy Lite

---
+++ src/main/java/net/kyrptonaught/serverutils/floodgateCompat/FabricProxyLite.java
@@ -0,0 +1,13 @@
+package net.kyrptonaught.serverutils.floodgateCompat;
+
+import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
+import one.oktw.VelocityLib;
+
+public class FabricProxyLite {
+
+    public static void hackEarlySendFix() {
+        ServerLoginConnectionEvents.QUERY_START.register((handler, server, sender, synchronizer) -> {
+            sender.sendPacket(VelocityLib.PLAYER_INFO_CHANNEL, VelocityLib.PLAYER_INFO_PACKET);
+        });
+    }
+}
--- src/main/java/net/kyrptonaught/serverutils/floodgateCompat/FloodgateCompatMod.java
+++ src/main/java/net/kyrptonaught/serverutils/floodgateCompat/FloodgateCompatMod.java
@@ -1,20 +1,14 @@
 package net.kyrptonaught.serverutils.floodgateCompat;
 
-import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.kyrptonaught.serverutils.Module;
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.util.Identifier;
 
 public class FloodgateCompatMod extends Module {
-    private static final Identifier PLAYER_INFO_CHANNEL = new Identifier("velocity", "player_info");
-    private static final PacketByteBuf PLAYER_INFO_PACKET = new PacketByteBuf(Unpooled.wrappedBuffer(new byte[]{4}).asReadOnly());
 
     @Override
     public void onInitialize() {
         //temp hack to fix compat with Fabric Proxy lite/floodgate/LuckPerms
-        ServerLoginConnectionEvents.QUERY_START.register((handler, server, sender, synchronizer) -> {
-            sender.sendPacket(PLAYER_INFO_CHANNEL, PLAYER_INFO_PACKET);
-        });
+        if (FabricLoader.getInstance().isModLoaded("fabricproxy-lite"))
+           FabricProxyLite.hackEarlySendFix();
     }
 }
