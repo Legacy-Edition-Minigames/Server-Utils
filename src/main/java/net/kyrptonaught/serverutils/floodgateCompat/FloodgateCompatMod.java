e4b0e6484accb3205913c604f04cb4016df74884
kyrptonaught <anthonybuttillo10@gmail.com>
2023-08-24 20:54:21 -0400

    Remove hard dependency on Fabric Proxy Lite

--- src/main/java/net/kyrptonaught/serverutils/floodgateCompat/FloodgateCompatMod.java
+++ src/main/java/net/kyrptonaught/serverutils/floodgateCompat/FloodgateCompatMod.java
@@ -1,16 +1,20 @@
 package net.kyrptonaught.serverutils.floodgateCompat;
 
+import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents;
 import net.kyrptonaught.serverutils.Module;
-import one.oktw.VelocityLib;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
 
 public class FloodgateCompatMod extends Module {
+    private static final Identifier PLAYER_INFO_CHANNEL = new Identifier("velocity", "player_info");
+    private static final PacketByteBuf PLAYER_INFO_PACKET = new PacketByteBuf(Unpooled.wrappedBuffer(new byte[]{4}).asReadOnly());
 
     @Override
     public void onInitialize() {
         //temp hack to fix compat with Fabric Proxy lite/floodgate/LuckPerms
         ServerLoginConnectionEvents.QUERY_START.register((handler, server, sender, synchronizer) -> {
-            sender.sendPacket(VelocityLib.PLAYER_INFO_CHANNEL, VelocityLib.PLAYER_INFO_PACKET);
+            sender.sendPacket(PLAYER_INFO_CHANNEL, PLAYER_INFO_PACKET);
         });
     }
 }
