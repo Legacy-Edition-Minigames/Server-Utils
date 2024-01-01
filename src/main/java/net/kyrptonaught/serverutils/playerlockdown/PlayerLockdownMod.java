e7160a52a0da45160471a030b04bd95bf51fe86e
kyrptonaught <anthonybuttillo10@gmail.com>
2024-01-01 16:19:27 -0500

    Seriously please stop jumping

--- src/main/java/net/kyrptonaught/serverutils/playerlockdown/PlayerLockdownMod.java
+++ src/main/java/net/kyrptonaught/serverutils/playerlockdown/PlayerLockdownMod.java
@@ -4,9 +4,11 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.kyrptonaught.serverutils.Module;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.network.packet.s2c.play.EntityStatusEffectS2CPacket;
+import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.RemoveEntityStatusEffectS2CPacket;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
@@ -56,11 +58,11 @@ public class PlayerLockdownMod extends Module {
             GLOBAL_LOCKDOWN = enabled;
             return 1;
         }
-        for (ServerPlayerEntity serverPlayerEntity : players) {
+        for (ServerPlayerEntity player : players) {
             if (enabled) {
-                LOCKEDDOWNPLAYERS.add(serverPlayerEntity.getUuidAsString());
+                LOCKEDDOWNPLAYERS.add(player.getUuidAsString());
             } else {
-                LOCKEDDOWNPLAYERS.remove(serverPlayerEntity.getUuidAsString());
+                LOCKEDDOWNPLAYERS.remove(player.getUuidAsString());
             }
         }
 
@@ -68,25 +70,30 @@ public class PlayerLockdownMod extends Module {
     }
 
     private static int executeFreeze(Collection<ServerPlayerEntity> players, boolean enabled) {
-        for (ServerPlayerEntity serverPlayerEntity : players) {
+        for (ServerPlayerEntity player : players) {
             if (enabled) {
-                serverPlayerEntity.getAbilities().setFlySpeed(0);
-                serverPlayerEntity.getAbilities().setWalkSpeed(0);
-                serverPlayerEntity.getAbilities().flying = false;
-                serverPlayerEntity.sendAbilitiesUpdate();
-                serverPlayerEntity.startFallFlying();
-                serverPlayerEntity.networkHandler.sendPacket(new EntityStatusEffectS2CPacket(serverPlayerEntity.getId(), new StatusEffectInstance(StatusEffects.JUMP_BOOST, -1, 250, false, false)));
+                player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue(0);
+                player.getAbilities().setFlySpeed(0);
+                player.getAbilities().setWalkSpeed(0);
+                player.getAbilities().flying = false;
+                player.sendAbilitiesUpdate();
+                player.startFallFlying();
+                player.networkHandler.sendPacket(new EntityStatusEffectS2CPacket(player.getId(), new StatusEffectInstance(StatusEffects.JUMP_BOOST, -1, 250, false, false)));
+                player.networkHandler.sendPacket(new HealthUpdateS2CPacket(player.getHealth(), 1, player.getHungerManager().getSaturationLevel()));
 
             } else {
-                serverPlayerEntity.getAbilities().setFlySpeed(.05f);
-                serverPlayerEntity.getAbilities().setWalkSpeed(.1f);
+                player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED).setBaseValue(.1f);
+                player.getAbilities().setFlySpeed(.05f);
+                player.getAbilities().setWalkSpeed(.1f);
+                player.sendAbilitiesUpdate();
 
-                StatusEffectInstance effect = serverPlayerEntity.getStatusEffect(StatusEffects.JUMP_BOOST);
+                StatusEffectInstance effect = player.getStatusEffect(StatusEffects.JUMP_BOOST);
                 if (effect != null) {
-                    serverPlayerEntity.networkHandler.sendPacket(new EntityStatusEffectS2CPacket(serverPlayerEntity.getId(), effect));
+                    player.networkHandler.sendPacket(new EntityStatusEffectS2CPacket(player.getId(), effect));
                 } else {
-                    serverPlayerEntity.networkHandler.sendPacket(new RemoveEntityStatusEffectS2CPacket(serverPlayerEntity.getId(), StatusEffects.JUMP_BOOST));
+                    player.networkHandler.sendPacket(new RemoveEntityStatusEffectS2CPacket(player.getId(), StatusEffects.JUMP_BOOST));
                 }
+                player.networkHandler.sendPacket(new HealthUpdateS2CPacket(player.getHealth(), player.getHungerManager().getFoodLevel(), player.getHungerManager().getSaturationLevel()));
             }
         }
 
