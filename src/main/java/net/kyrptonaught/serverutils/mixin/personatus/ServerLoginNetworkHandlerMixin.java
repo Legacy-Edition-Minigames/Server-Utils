b1f21ddd4dd210c6e6d47c18f50b84761ad61ca3
kyrptonaught <anthonybuttillo10@gmail.com>
2024-01-17 02:17:54 -0500

    Update dependencies

--- src/main/java/net/kyrptonaught/serverutils/mixin/personatus/ServerLoginNetworkHandlerMixin.java
+++ src/main/java/net/kyrptonaught/serverutils/mixin/personatus/ServerLoginNetworkHandlerMixin.java
@@ -1,48 +1,44 @@
 package net.kyrptonaught.serverutils.mixin.personatus;
 
 import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.yggdrasil.ProfileResult;
 import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
 import com.mojang.util.UndashedUuid;
-import net.fabricmc.fabric.mixin.networking.accessor.ServerLoginNetworkHandlerAccessor;
 import net.kyrptonaught.serverutils.personatus.PersonatusModule;
 import net.kyrptonaught.serverutils.personatus.PersonatusProfile;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
-import org.spongepowered.asm.mixin.Final;
+import one.oktw.mixin.core.ServerLoginNetworkHandlerAccessor;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.UUID;
 
-@Mixin(targets = "net.minecraft.server.network.ServerLoginNetworkHandler$1")
+@Mixin(targets = "one.oktw.PacketHandler")
 public abstract class ServerLoginNetworkHandlerMixin {
 
 
-    @Shadow
-    @Final
-    ServerLoginNetworkHandler field_14176;
+    @Redirect(method = "lambda$handleVelocityPacket$0", at = @At(value = "INVOKE", target = "Lone/oktw/mixin/core/ServerLoginNetworkHandlerAccessor;setProfile(Lcom/mojang/authlib/GameProfile;)V"))
+    public void requestSpoof(ServerLoginNetworkHandlerAccessor login, GameProfile profile, PacketByteBuf buf, ServerLoginNetworkHandler handler) {
+        if (!PersonatusModule.isEnabled()) {
+            login.setProfile(profile);
+            return;
+        }
 
-    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lcom/mojang/authlib/yggdrasil/ProfileResult;profile()Lcom/mojang/authlib/GameProfile;"))
-    public GameProfile requestSpoof(ProfileResult instance) {
-        if (PersonatusModule.isEnabled()) {
-            GameProfile oldProfile = instance.profile();
-            if (((ServerLoginNetworkHandlerAccessor) field_14176).getServer().getSessionService() instanceof YggdrasilMinecraftSessionService sessionService) {
-                String responseName = PersonatusModule.URLGetValue(false, "kvs/get/personatus/" + oldProfile.getName(), "value");
-                if (responseName != null) {
-                    String responseUUID = PersonatusModule.URLGetValue(true, "https://api.mojang.com/users/profiles/minecraft/" + responseName, "id");
-                    if (responseUUID != null) {
-                        UUID uuid = UndashedUuid.fromString(responseUUID);
-                        GameProfile spoofed = sessionService.fetchProfile(uuid, true).profile();
+        MinecraftServer server = ((net.fabricmc.fabric.mixin.networking.accessor.ServerLoginNetworkHandlerAccessor) handler).getServer();
+        if (server.getSessionService() instanceof YggdrasilMinecraftSessionService sessionService) {
+            String responseName = PersonatusModule.URLGetValue(false, "kvs/get/personatus/" + profile.getName(), "value");
+            if (responseName != null) {
+                String responseUUID = PersonatusModule.URLGetValue(true, "https://api.mojang.com/users/profiles/minecraft/" + responseName, "id");
+                if (responseUUID != null) {
+                    UUID uuid = UndashedUuid.fromString(responseUUID);
+                    GameProfile spoofed = sessionService.fetchProfile(uuid, true).profile();
 
-                        ((PersonatusProfile) spoofed).setRealProfile(oldProfile);
-                        return spoofed;
-                    }
+                    ((PersonatusProfile) spoofed).setRealProfile(profile);
+                    login.setProfile(spoofed);
                 }
             }
         }
-
-        return instance.profile();
     }
 }
