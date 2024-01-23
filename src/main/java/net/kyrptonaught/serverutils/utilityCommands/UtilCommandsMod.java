d4bb6b6b2d2fc0337d9806f4f69cab0e13d69131
kyrptonaught <anthonybuttillo10@gmail.com>
2024-01-22 19:42:18 -0500

    Stop max health attribute from syncing automatically

--- src/main/java/net/kyrptonaught/serverutils/utilityCommands/UtilCommandsMod.java
+++ src/main/java/net/kyrptonaught/serverutils/utilityCommands/UtilCommandsMod.java
@@ -1,22 +1,35 @@
 package net.kyrptonaught.serverutils.utilityCommands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import net.kyrptonaught.serverutils.Module;
 import net.minecraft.command.argument.BlockPosArgumentType;
 import net.minecraft.command.argument.CommandFunctionArgumentType;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.network.packet.s2c.play.EntityAttributesS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.FunctionCommand;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.function.CommandFunction;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class UtilCommandsMod extends Module {
+
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+        EntityAttributes.GENERIC_MAX_HEALTH.setTracked(false);
+    }
 
     @Override
     public void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -65,5 +78,25 @@ public class UtilCommandsMod extends Module {
                                     return 1;
                                 }))));
 
+        dispatcher.register(CommandManager.literal("maxHealthUpdate")
+                .requires((source) -> source.hasPermissionLevel(2))
+                .then(CommandManager.argument("player", EntityArgumentType.players())
+                        .then(CommandManager.argument("entity", EntityArgumentType.entities())
+                                .executes(context -> {
+                                    Collection<ServerPlayerEntity> players = EntityArgumentType.getPlayers(context, "player");
+                                    Collection<? extends Entity> entities = EntityArgumentType.getEntities(context, "entity");
+
+                                    for (ServerPlayerEntity serverPlayerEntity : players) {
+                                        for (Entity entity : entities) {
+                                            if (!(entity instanceof LivingEntity)) {
+                                                throw new DynamicCommandExceptionType(name -> Text.stringifiedTranslatable("commands.attribute.failed.entity", name)).create(entity.getName());
+                                            }
+
+                                            serverPlayerEntity.networkHandler.sendPacket(new EntityAttributesS2CPacket(entity.getId(), Collections.singleton(((LivingEntity) entity).getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH))));
+                                        }
+                                    }
+
+                                    return 1;
+                                }))));
     }
 }
